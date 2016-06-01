247d6e6b31143111105c9ca312166aaddcd53d0e
Zane Mountcastle <zane.mountcastle@gmail.com>
2016-06-01 09:35:40 +0800

    Add Countdown Form component

---
+++ app/styles/components/CountdownForm.test.jsx
@@ -0,0 +1,13 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var CountdownForm = require('CountdownForm');
+
+describe('CountdownForm', () =>{
+  it('should exist'){
+    
+  }
+});
